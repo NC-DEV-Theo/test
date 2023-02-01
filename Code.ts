de70ff39ed4efabcfc456e2bc274fb2468aa655f
Théo Reumont <theo.reumont@numericoach.fr>
2023-02-01 09:36:14 +0100

    Test linting on function

--- Code.ts
+++ Code.ts
@@ -13,3 +13,15 @@ function myFunction(){
 function newFunction(){
     console.log("Hello GitHub")
 }
+
+function thisIsNewFunction(){
+    const SS = SpreadsheetApp.getActiveSpreadsheet();
+    const sheet = SS.getSheetByName('Sheet1');
+    if(!sheet) throw new Error('Sheet1 not found');
+    const range = sheet.getRange('A1:A');
+    const values = range.getValues();
+    const values2 = values.filter(function(value){
+        return value[0] !== '';
+    })
+    console.log(values2);
+}
